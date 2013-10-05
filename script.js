b67899ff76bfa3258cac0c9662bfca1416d51e5a
Brenan Klain <brenanklain@gmail.com>
2013-10-05 16:25:04 -0600

    Creates Model and starts View

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+	var Card = Backbone.Model.extend({
+		defaults:{
+			value: null,
+			suit:null,
+			str_value:null
+		}
+		
+	});
+
+	var CardView = Backbone.Model.extend({
+		template: _.template($("#card-template")).html(),
+		render: function(){
+			this.$el.html(this.template(this.Model.toJSON()));
+		}
+	});
+
+
+
 	function convert_value_to_string(value) {
 		if (value > 10) {
 			switch (value) {
@@ -21,8 +39,24 @@ $(document).ready(function() {
 	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 	for (var i = 0; i<suits.length; i++) {
 		var suit = suits[i];
+
 		for (var j = 0; j<13; j++) {
-			deck.push({value:j+1, suit: suit});
+			var createCard = new Card({
+				value:j+1,
+				suit: suit,
+				str_value: convert_value_to_string(j+1)
+
+			});
+			deck.push(createCard);
 		}
 	}
+	_.shuffle(deck);
+
+	$("button").click(function(){
+		var myCardView = new CardView({el: $("#my-card")});
+		var oppCardView = new CardView({el: $("#opp-card")});
+		
+	});
+
+
 });
